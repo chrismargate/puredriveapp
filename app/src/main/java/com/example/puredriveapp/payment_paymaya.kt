e66e60bce9d0f403a119dd425a4e98655d94dab1
JemmalynDeLeon <jemmaadeleon@gmail.com>
2022-09-10 03:32:32 +0800

    payment to upload file pages

---
+++ app/src/main/java/com/example/puredriveapp/payment_paymaya.kt
@@ -0,0 +1,14 @@
+package com.example.puredriveapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class payment_paymaya : AppCompatActivity ()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.payment_paymaya)
+
+
+    }
+}
