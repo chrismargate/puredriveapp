77ed2ab81e551da00d5e408c1667cccb0bdde25c
JemmalynDeLeon <jemmaadeleon@gmail.com>
2022-09-22 02:20:25 +0800

    new XML files but editing file names and doing fragments

--- app/src/main/java/com/example/puredriveapp/fragments/RentalSignUp2Fragment.kt
+++ app/src/main/java/com/example/puredriveapp/fragments/RentalSignUp2Fragment.kt
@@ -15,10 +15,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentRentalSignUp2.newInstance] factory method to
+ * Use the [RentalSignUp2Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentRentalSignUp2 : Fragment() {
+class RentalSignUp2Fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
--- app/src/main/java/com/example/puredriveapp/fragments/RentalSignUp3Fragment.kt
+++ app/src/main/java/com/example/puredriveapp/fragments/RentalSignUp3Fragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentRentalSignUp3.newInstance] factory method to
+ * Use the [RentalSignUp3Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentRentalSignUp3 : Fragment() {
+class RentalSignUp3Fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
