747425abdb84b1d392ca635c68a180f0aeb88eb2
JemmalynDeLeon <jemmaadeleon@gmail.com>
2022-09-27 05:10:48 +0800

    FINAL navbar for staff

---
+++ app/src/main/java/com/example/puredriveapp/ActivityStaffMain.kt
@@ -0,0 +1,11 @@
+package com.example.puredriveapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityStaffMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_staff_main)
+    }
+}
