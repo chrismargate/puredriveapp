b2b756d28e17127914e6c72307c255e2d10dcbf3
margate23 <78247006+chrismargo@users.noreply.github.com>
2022-09-28 04:41:04 +0800

    added qrcodeactivity

---
+++ app/src/main/java/com/example/puredriveapp/QRCodeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.puredriveapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class QRCodeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrcode)
+    }
+}
