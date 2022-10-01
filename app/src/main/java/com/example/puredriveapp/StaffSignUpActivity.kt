6371321af39e2ab56c23bb7ff30cb88b1f2ea755
margate23 <78247006+chrismargo@users.noreply.github.com>
2022-10-01 22:08:07 +0800

    temporarily removed view binding

--- app/src/main/java/com/example/puredriveapp/StaffSignUpActivity.kt
+++ app/src/main/java/com/example/puredriveapp/StaffSignUpActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.example.puredriveapp.databinding.ActivityStaffSignupBinding
 import com.example.puredriveapp.dataclasses.Staff
 import com.example.puredriveapp.dataclasses.User
 import com.example.puredriveapp.fragments.signup.staff.StaffSignUp1Fragment
@@ -12,13 +11,13 @@ import com.example.puredriveapp.interfaces.StaffSignUpCommunicator
 
 class StaffSignUpActivity : AppCompatActivity(), StaffSignUpCommunicator{
 
-    private lateinit var binding: ActivityStaffSignupBinding
+    //private lateinit var binding: ActivityStaffSignupBinding
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        binding = ActivityStaffSignupBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        //setContentView(R.layout.activity_staff_signup)
+        //binding = ActivityStaffSignupBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
+        setContentView(R.layout.activity_staff_signup)
 
         supportFragmentManager.beginTransaction().replace(R.id.frameStaffSignUp,StaffSignUp1Fragment()).commit()
     }
